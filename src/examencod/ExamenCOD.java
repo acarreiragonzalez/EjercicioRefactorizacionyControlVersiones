41394ec452e9d3561ed2d004f99ae8487ce8ae66
acarreiragonzalez <acarreiragonzalez@eniac25.danielcastelao.org>
2014-12-04 09:03:10 +0100

    Commit metodo renombrado

--- src/examencod/ExamenCOD.java
+++ src/examencod/ExamenCOD.java
@@ -7,20 +7,20 @@ public class ExamenCOD {
      */
     public static void main(String[] args) {
         int renombrada = 11;
-        if (metodoMio(renombrada)) {
+        if (metodoRenombrado(renombrada)) {
             System.out.println("Es numero primo");
         } else {
             System.out.println("NO es numero primo");
         }
 
         for (int x = 2; x < 1000; x++) {
-            if (metodoMio(x)) {
+            if (metodoRenombrado(x)) {
                 System.out.print(x + " ");
             }
         }
     }
 
-    public static boolean metodoMio(int variable) {
+    public static boolean metodoRenombrado(int variable) {
         int j = 2;
         boolean renombrada2 = true;
         while ((renombrada2) && (j != variable)) {
